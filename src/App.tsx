fb5e1f2155976af7f929026ffe82930acb0e128f
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-04-27 17:04:39 -0300

    feat: :sparkles: add function to show total number of tasks

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,21 @@ function App() {
 
   const [task, setTask] = useState("");
 
+  const [totalTasks, setTotalTasks] = useState(toDoList.length);
+
   function handleAddTask(event: ChangeEvent<HTMLInputElement>) {
-    const newTask = event.target.value;
-    setTask(newTask);
+    setTask(event.target.value);
   }
 
   function handleNewTask() {
-    setToDoList([...toDoList, task]);
-    setTask("");
+    if (task !== "") {
+      setToDoList((prevState) => {
+        const newToDoList = [...prevState, task];
+        setTotalTasks(newToDoList.length);
+        return newToDoList;
+      });
+      setTask("");
+    }
   }
 
   return (
@@ -33,6 +40,7 @@ function App() {
         <div className="flex justify-between gap-2 h-14">
           <input
             type="text"
+            value={task}
             onChange={handleAddTask}
             placeholder="Adicione uma nova tarefa"
             className="bg-slate-700 w-full px-4 rounded-lg"
@@ -45,33 +53,37 @@ function App() {
           </button>
         </div>
 
-        <div className="flex justify-between items-center border-b-[1px] border-slate-500/50 pb-6 text-blue-400 font-semibold ">
-          <p>
-            Tarefas criadas{" "}
-            <span className="bg-slate-900 py-1 px-3 rounded-full">0</span>
-          </p>
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-between items-center border-b-[1px] border-slate-500/50 pb-6 text-blue-400 font-semibold ">
+            <p>
+              Tarefas criadas{" "}
+              <span className="bg-slate-900 py-1 px-3 rounded-full">
+                {totalTasks}
+              </span>
+            </p>
 
-          <p>
-            Concluídas{" "}
-            <span className="bg-slate-900 py-1 px-3 rounded-full">0</span>
-          </p>
-        </div>
-
-        {haveList ? (
-          <div className="flex flex-col gap-3">
-            {toDoList.map((task) => (
-              <ToDoItem task={task} />
-            ))}
+            <p>
+              Concluídas{" "}
+              <span className="bg-slate-900 py-1 px-3 rounded-full">0</span>
+            </p>
           </div>
-        ) : (
-          <div className="flex flex-col items-center gap-4 opacity-50">
-            <ListX size={70} />
-            <div>
-              <strong>Você ainda não tem tarefas cadastradas</strong>
-              <p>Crie tarefas e organize seus itens a fazer</p>
+
+          {haveList ? (
+            <div className="flex flex-col gap-3">
+              {toDoList.map((task) => (
+                <ToDoItem task={task} />
+              ))}
             </div>
-          </div>
-        )}
+          ) : (
+            <div className="flex flex-col items-center gap-4 opacity-50">
+              <ListX size={70} />
+              <div>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            </div>
+          )}
+        </div>
       </main>
     </div>
   );
